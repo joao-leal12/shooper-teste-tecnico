af8d15ed4e7e6480052619b072ce366f873460ae
João Victor <97269781+joao-leal12@users.noreply.github.com>
2024-11-28 00:34:24 -0300

    Remover todos os arquivos do repositório remoto

--- src/components/Input/schemas/apis-schemas.ts
+++
@@ -1,8 +0,0 @@
-import yup from 'yup'
-
-export const sendLocalizationPropsSchema = yup.object({ 
-    customer_id: yup.string().uuid().required(), 
-    origin: yup.string().required(),
-    destination: yup.string().required()
-
-})
--- src/infra/http/clientRequest.ts
+++
@@ -1,4 +0,0 @@
-export interface ClientRequest<TData, TDataBody>{ 
-    get: (url: string, params?: string, queryParams?: string) => TData[]
-    post: (url : string, body: string, params?: string, queryParams?: string) => TDataBody 
-}
